7fa62da881c11df2dd611e5650240a704d0732f7
Simon Prickett <simon@redislabs.com>
2022-01-13 17:08:46 +0000

    Linter fixes.

--- aredis_om/checks.py
+++ aredis_om/checks.py
@@ -9,12 +9,14 @@ async def check_for_command(conn, cmd):
     cmd_info = await conn.execute_command("command", "info", cmd)
     return not None in cmd_info
 
+
 @lru_cache(maxsize=None)
 async def has_redis_json(conn=None):
     if conn is None:
         conn = get_redis_connection()
     command_exists = await check_for_command(conn, "json.set")
     return command_exists
+
 
 @lru_cache(maxsize=None)
 async def has_redisearch(conn=None):
