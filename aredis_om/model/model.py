a8cfd5ee2f5b352208791be9928705f4e8ce3de0
Serhii Charykov <laammaar@gmail.com>
2022-05-06 20:09:51 +0300

    Change RedisModel.from_redis to Python 3 syntax from six; Format code

--- aredis_om/model/model.py
+++ aredis_om/model/model.py
@@ -1179,14 +1179,10 @@ class RedisModel(BaseModel, abc.ABC, metaclass=ModelMeta):
     @classmethod
     def from_redis(cls, res: Any):
         # TODO: Parsing logic copied from redisearch-py. Evaluate.
-        import six
-        from six.moves import xrange
-        from six.moves import zip as izip
-
         def to_string(s):
-            if isinstance(s, six.string_types):
+            if isinstance(s, str):
                 return s
-            elif isinstance(s, six.binary_type):
+            elif isinstance(s, bytes):
                 return s.decode("utf-8", "ignore")
             else:
                 return s  # Not a string we care about
@@ -1195,12 +1191,12 @@ class RedisModel(BaseModel, abc.ABC, metaclass=ModelMeta):
         step = 2  # Because the result has content
         offset = 1  # The first item is the count of total matches.
 
-        for i in xrange(1, len(res), step):
+        for i in range(1, len(res), step):
             fields_offset = offset
 
             fields = dict(
                 dict(
-                    izip(
+                    zip(
                         map(to_string, res[i + fields_offset][::2]),
                         map(to_string, res[i + fields_offset][1::2]),
                     )
